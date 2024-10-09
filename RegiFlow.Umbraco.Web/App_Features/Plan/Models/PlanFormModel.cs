78849ca2cafe7956a194b3894d56f0fc4b638ad3
hbux <hb740@outlook.com>
2024-10-09 22:17:04 +0100

    Add payment plan UI

--- RegiFlow.Umbraco.Web/App_Features/Plan/Models/PlanFormModel.cs
+++ RegiFlow.Umbraco.Web/App_Features/Plan/Models/PlanFormModel.cs
@@ -1,6 +1,13 @@
-﻿namespace RegiFlow.Umbraco.Web.App_Features.Plan.Models
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegiFlow.Umbraco.Web.App_Features.Plan.Models
 {
     public class PlanFormModel
     {
+        [Required(ErrorMessage = "Please choose a plan")]
+        [DisplayName("Choose a plan")]
+        public Guid Plan { get; set; }
     }
 }
